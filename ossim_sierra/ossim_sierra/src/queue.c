80833c1fc06db7eeb154ce3d5da0790127d3b33a
ken-nguyen-2605 <josephken020605@gmail.com>
2025-04-04 00:20:55 +0700

    feat: sched.c queue.c, add .gitignore, remove *.o and os

--- ossim_sierra/ossim_sierra/src/queue.c
+++ ossim_sierra/ossim_sierra/src/queue.c
@@ -9,12 +9,27 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+        if (q == NULL || proc == NULL) return;
+        if (q->size < MAX_QUEUE_SIZE) {
+                q->proc[q->size] = proc;
+                q->size++;
+        }
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
+        if (q == NULL) return;
+        if (q->size == 0) return NULL;
+
+        struct pcb_t * pcb = q->proc[0];
+        for (int i = 1; i < q->size; i++) {
+                q->proc[i - 1] = q->proc[i];
+        }
+        q->size--;
+        q->proc[q->size] = NULL; // Clear the last element
+
+        return pcb;
 }
 
